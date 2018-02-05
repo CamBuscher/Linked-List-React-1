84b96b3916303d73987751d81de391b16dd9f81b
KyleeC <kyleewellons@gmail.com>
2018-02-05 14:42:01 -0700

    Add javascript psudeocode

---
+++ script.js
@@ -0,0 +1,28 @@
+// // <h1 class="important-header">Dinosaurs are awesome.</h1>
+
+// // <input class="new-text" value="Wow">
+
+// // <button class="change-me">Change Me</button>
+
+
+
+// // Grab the user input from two i.d's and append them to the list area section upon a button click
+
+
+// function listItem (){
+// var userTitle = document.querySelector('#user-title').value;
+
+// var userUrl = document.querySelector('#user-url').value;
+// // $('#list-item').append(
+// // <article class="list-item">
+// //         <h2>userTitle</h2>
+// //         <hr>
+// //         <p><a href ="userUrl" class="link-list" target="_blank">
+// //         userUrl</a></p>
+// //         <hr>
+// //         <button class="read-button">Read</button>
+// //         <button class="delete-button">Delete</button>
+// //       </article>);
+
+// console.log (userTitle)
+// };
