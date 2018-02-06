7afa7f738c1356227039dec92649042df7adfb06
Cameron Buscher <cameronbuscher@gmail.com>
2018-02-05 18:00:16 -0700

    Added bookmark functionality
    Complete with read button functionality and delete button functionality. All work as they should. Next steps are responsive design and modifying input field functionality

--- script.js
+++ script.js
@@ -9,20 +9,39 @@
 // // Grab the user input from two i.d's and append them to the list area section upon a button click
 
 
-// function listItem (){
-// var userTitle = document.querySelector('#user-title').value;
+function listItem (){
+  var userTitle = document.querySelector('#user-title').value;
+  var userUrl = document.querySelector('#user-url').value;
+  $('#list-area').append(`
+    <article class="list-item">
+      <h2>${userTitle}</h2>
+      <hr>
+      <p><a href="http://${userUrl}" class="link-list" target="_blank">
+      ${userUrl}</a></p>
+      <hr>
+      <button class="read-button">Read</button>
+      <button class="delete-button">Delete</button>
+    </article>`);
+};
 
-// var userUrl = document.querySelector('#user-url').value;
-// // $('#list-item').append(
-// // <article class="list-item">
-// //         <h2>userTitle</h2>
-// //         <hr>
-// //         <p><a href ="userUrl" class="link-list" target="_blank">
-// //         userUrl</a></p>
-// //         <hr>
-// //         <button class="read-button">Read</button>
-// //         <button class="delete-button">Delete</button>
-// //       </article>);
+$('#enter-button').on('click', function() {
+  listItem();
+  event.preventDefault();
+});
 
-// console.log (userTitle)
-// };
+$('#list-area').on('click', '.read-button', readButtonToggle);
+
+function readButtonToggle() {
+  $(this).closest('article').find('.read-button').toggleClass('read-button-read');
+  $(this).closest('article').toggleClass('list-item-read');
+  $(this).closest('article').find('.delete-button').toggleClass('delete-button-read');
+  $(this).closest('article').find('.link-list').toggleClass('link-list-read');
+};
+
+$('#list-area').on('click', '.delete-button', removeArticle);
+
+function removeArticle() {
+  $(this).closest('article').remove('article');
+}
+
+
